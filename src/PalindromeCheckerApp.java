67fa45b6821d48ed52e503af18c6742d462cd97b
dm2883-png <dm2883@srmist.edu.in>
2026-02-26 09:44:57 +0530

    UC67

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,7 @@
 import java.util.Deque;
 import java.util.ArrayDeque;
 
-public class UseCase7PalindromeCheckerApp {
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
